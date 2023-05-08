b34ed7f2d22d1f6ec8f38787231a4ebf7991b944
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-08 18:48:15 -0500

    New Project task 0

---
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10, 10);
+        Console.Write(number);
+        if (number > 0)
+        {
+            Console.Write(" is positive");
+        }
+        else if (number == 0)
+        {
+            Console.Write(" is zero");
+        }
+        else
+        {
+            Console.Write(" is negative");
+        }
+    }
+}
