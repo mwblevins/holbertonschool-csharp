c2355a91e895330674c2407f1b89d8c1d32acc3f
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-08 16:54:36 -0500

    Task 3

---
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Schoolio
+{
+    public static void Main()
+    {
+        Console.WriteLine("\"Programming is like building a multilingual puzzle");
+        Console.WriteLine();
+    }
+}
