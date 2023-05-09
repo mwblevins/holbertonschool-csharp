132298d9239ce12dd54881a134c870c5ccd71d9a
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-09 11:58:45 -0500

    Task 8

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int lastDigit = Math.Abs(number % 10);
+        Console.WriteLine(lastDigit);
+        return lastDigit;
+    }
+}
