7273f03c5b4139000d82bc152ae8a9861e71495e
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-08 19:08:09 -0500

    tAsk 2

---
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char alpha = 'a'; alpha <= 'z'; alpha++)
+        {
+            Console.Write(alpha);
+        }
+    }
+}
