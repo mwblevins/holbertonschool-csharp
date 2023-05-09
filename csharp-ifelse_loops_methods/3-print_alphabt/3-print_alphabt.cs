23a6c00f320befda3214e51ac8b43eeb53a35350
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-09 11:15:48 -0500

    Task 3

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char x = 'a'; x <= 'z'; x++)
+        {
+            if (x != 'q' && x != 'e')
+            {
+                Console.Write(x);
+            }
+        }
+    }
+}
