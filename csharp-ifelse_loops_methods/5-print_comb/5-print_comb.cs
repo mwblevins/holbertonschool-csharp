89615322464e9a46b4b2ea39833f584796c4a4cd
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-09 11:32:15 -0500

    Task 5

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string output = "";
+        for (int i = 0; i < 100; i++)
+        {
+            if (i < 10)
+            {
+                output += "0";
+            }
+            output += $"{i}";
+            if (i < 99)
+            {
+                output += ", ";
+            }
+        }
+        Console.Write(output + "\n");
+    }
+}
