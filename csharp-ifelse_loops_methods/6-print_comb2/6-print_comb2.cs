fa778adcdc899160ec984e8d162bdf0c4716412a
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-09 11:49:26 -0500

    task 6

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i <= 8; i++)
+        {
+            for (int j = i + 1; j <= 9; j++)
+            {
+                Console.Write("{0}{1}", i, j);
+                if (i != 8 || j != 9)
+                {
+                    Console.Write(", ");
+                }
+            }
+        }
+        Console.WriteLine();
+    }
+}
