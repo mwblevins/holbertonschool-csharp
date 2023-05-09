2ca1d8f57d436b5272db2c5f077e8137ccfc7d13
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-09 11:21:24 -0500

    Task 4

---
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 99; i++)
+        {
+            Console.Write("{0} = 0x{1:X}\n", i, i);
+        }
+    }
+}
