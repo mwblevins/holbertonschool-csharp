6e1ddf8627775749997c3ee049b81497bc9ec681
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-10 15:43:19 -0500

    task 4

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        List<int> newList = new List<int>();
+
+        for (int i = 0; i < size; i++)
+        {
+            newList.Add(i);
+            Console.Write(i + " ");
+        }
+
+        Console.WriteLine();
+
+        return newList;
+    }
+}
