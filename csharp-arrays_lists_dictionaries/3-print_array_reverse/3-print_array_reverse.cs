459f237e774a89c0cce7828960758339eb136229
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-10 15:20:08 -0500

    task 3

---
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+    }
+}
