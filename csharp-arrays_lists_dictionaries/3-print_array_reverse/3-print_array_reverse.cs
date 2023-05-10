7d100c414dc60a20ae25af5948dbc834d0fb1498
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-10 15:40:44 -0500

    wrong location

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -4,9 +4,9 @@ class Array
 {
     public static void Reverse(int[] array)
     {
+        int x = (array?.Length).GetValueOrDefault(0);
         for (int i = x - 1; i != -1; i--)
         {
-            int x = (array?.Length).GetValueOrDefault(0);
             Console.Write("{0}{1}", array[i], i == 0 ? "" : " ");
         }
         Console.WriteLine();
