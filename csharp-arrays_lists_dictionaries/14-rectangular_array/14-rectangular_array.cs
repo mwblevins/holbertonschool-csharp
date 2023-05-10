fb2a07b09ad6259fd5805539cc9bc548173aafc9
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-10 17:59:31 -0500

    task 14

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] array = new int[5, 5];
+
+        // initialize all indices to 0
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                array[i, j] = 0;
+            }
+        }
+
+        // set index [2,2] to 1
+        array[2, 2] = 1;
+
+        // print the array
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write("{0} ", array[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
