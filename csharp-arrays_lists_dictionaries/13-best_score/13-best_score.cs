1cc0ccc004a298fbf6cd8ba033bd79ce104f6eb5
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-10 17:56:20 -0500

    task 13

--- csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int maxScore = -1;
+        string bestStudent = "None";
+
+        foreach (KeyValuePair<string, int> kvp in myList)
+        {
+            if (kvp.Value > maxScore)
+            {
+                maxScore = kvp.Value;
+                bestStudent = kvp.Key;
+            }
+        }
+
+        return bestStudent;
+    }
+}
