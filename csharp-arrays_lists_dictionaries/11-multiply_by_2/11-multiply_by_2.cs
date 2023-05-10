e9c3a3dab2299b6b7da988a21d34e85aa73ef022
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-10 17:43:04 -0500

    task 11

---
+++ csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        Dictionary<string, int> result = new Dictionary<string, int>();
+
+        foreach (KeyValuePair<string, int> kvp in myDict)
+        {
+            result[kvp.Key] = kvp.Value * 2;
+        }
+
+        return result;
+    }
+}
