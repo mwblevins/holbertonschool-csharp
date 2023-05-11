dac652257974989e76543e2fe982337ae0964a31
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-11 14:15:56 -0500

    Task 3

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: " + aStack.Count);
+        
+        if (aStack.Count > 0)
+        {
+            Console.WriteLine("Top item: " + aStack.Peek());
+        }
+        else
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        
+        bool containsSearch = aStack.Contains(search);
+        Console.WriteLine("Stack contains " + search + ": " + containsSearch);
+
+        if (containsSearch)
+        {
+            Stack<string> tempStack = new Stack<string>();
+            bool foundSearch = false;
+
+            while (aStack.Count > 0)
+            {
+                string item = aStack.Pop();
+                if (item == search)
+                {
+                    foundSearch = true;
+                    break;
+                }
+                tempStack.Push(item);
+            }
+
+            if (!foundSearch)
+            {
+                while (tempStack.Count > 0)
+                {
+                    aStack.Push(tempStack.Pop());
+                }
+            }
+        }
+
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
