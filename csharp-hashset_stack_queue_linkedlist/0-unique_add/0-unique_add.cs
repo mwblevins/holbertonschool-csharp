258327fc1e71aa59f90de81b8fd8bfc767cbf9c3
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-11 14:07:53 -0500

    task 0

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class List
+{
+    public static int Sum(List<int> myList)
+    {
+        // Create a dictionary to keep track of the number of occurrences of each integer
+        Dictionary<int, int> counts = new Dictionary<int, int>();
+
+        // Loop through each integer in the list and increment its count in the dictionary
+        foreach (int num in myList)
+        {
+            if (counts.ContainsKey(num))
+            {
+                counts[num]++;
+            }
+            else
+            {
+                counts[num] = 1;
+            }
+        }
+
+        // Calculate the sum of unique integers by adding up the integers that occur only once
+        int sum = 0;
+        foreach (KeyValuePair<int, int> kvp in counts)
+        {
+            if (kvp.Value == 1)
+            {
+                sum += kvp.Key;
+            }
+        }
+
+        return sum;
+    }
+}
