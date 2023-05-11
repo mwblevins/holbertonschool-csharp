7699f39908616c81b59c063c45aa1ba9dec4f9e3
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-11 14:58:19 -0500

    Task 8

---
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int index = 0;
+        LinkedListNode<int> currentNode = myLList.First;
+
+        while (currentNode != null)
+        {
+            if (currentNode.Value == value)
+            {
+                return index;
+            }
+            currentNode = currentNode.Next;
+            index++;
+        }
+
+        return -1;
+    }
+}
+
