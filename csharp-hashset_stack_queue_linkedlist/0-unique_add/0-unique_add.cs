eb9447dcf397b5a5c0204a5e00631b40d55b74cc
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-11 14:17:45 -0500

    Task 0, handle negatives?

--- csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -1,37 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+
 
 public class List
 {
     public static int Sum(List<int> myList)
     {
-        // Create a dictionary to keep track of the number of occurrences of each integer
-        Dictionary<int, int> counts = new Dictionary<int, int>();
-
-        // Loop through each integer in the list and increment its count in the dictionary
-        foreach (int num in myList)
-        {
-            if (counts.ContainsKey(num))
-            {
-                counts[num]++;
-            }
-            else
-            {
-                counts[num] = 1;
-            }
-        }
-
-        // Calculate the sum of unique integers by adding up the integers that occur only once
+        HashSet<int> unique = new HashSet<int>();
         int sum = 0;
-        foreach (KeyValuePair<int, int> kvp in counts)
+
+        foreach (int i in myList)
         {
-            if (kvp.Value == 1)
+            if (!unique.Contains(i))
             {
-                sum += kvp.Key;
+                unique.Add(i);
+                sum += i;
             }
         }
-
         return sum;
     }
 }
