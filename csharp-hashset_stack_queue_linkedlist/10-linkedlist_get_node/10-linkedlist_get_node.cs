2de8ebaeb6bfd37863039478c6d5f50809a7ddba
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-11 15:04:27 -0500

    Task 10

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        if (n < 0 || n >= myLList.Count)
+        {
+            return 0;
+        }
+
+        int i = 0;
+        foreach (int value in myLList)
+        {
+            if (i == n)
+            {
+                return value;
+            }
+            i++;
+        }
+
+        return 0;
+    }
+}
