5105c66d140ad16186e46765a2dd53386131a328
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-11 14:49:22 -0500

    task 6

---
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int count = 0;
+        LinkedListNode<int> currentNode = myLList.First;
+        while (currentNode != null)
+        {
+            count++;
+            currentNode = currentNode.Next;
+        }
+        return count;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        LinkedList<int> llist = new LinkedList<int>();
+        llist.AddLast(1);
+        llist.AddLast(2);
+        llist.AddLast(3);
+
+        int length = LList.Length(llist);
+
+        Console.WriteLine("Linked List Length: " + length);
+    }
+}
