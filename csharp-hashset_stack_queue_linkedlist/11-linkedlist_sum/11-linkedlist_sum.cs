7664a38f72f1231d6337ecf99bb6f03776e3d0c6
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-11 15:06:24 -0500

    task 11

---
+++ csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int sum = 0;
+        LinkedListNode<int> currentNode = myLList.First;
+
+        while (currentNode != null)
+        {
+            sum += currentNode.Value;
+            currentNode = currentNode.Next;
+        }
+
+        return sum;
+    }
+}
