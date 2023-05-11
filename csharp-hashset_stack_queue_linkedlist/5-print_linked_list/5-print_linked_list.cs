8e1181bc17302d6ea2c65cb9f811fa23f57ba56d
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-11 14:47:30 -0500

    Task 5

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linked_list/5-print_linked_list.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> list = new LinkedList<int>();
+        if (size < 0)
+        {
+            return list;
+        }
+        for (int i = 0; i < size; i++)
+        {
+            list.AddLast(i);
+            Console.WriteLine(i);
+        }
+        return list;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        LinkedList<int> llist;
+        int size = 10;
+
+        llist = LList.CreatePrint(size);
+
+        Console.WriteLine("Linked List Length: " + llist.Count);
+    }
+}
