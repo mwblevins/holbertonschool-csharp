0e07a056e3eb284bcfb521ba0c782ec9984cfa87
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-11 14:21:28 -0500

    too many POP

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -6,7 +6,7 @@ public class MyStack
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
         Console.WriteLine("Number of items: " + aStack.Count);
-        
+
         if (aStack.Count > 0)
         {
             Console.WriteLine("Top item: " + aStack.Peek());
@@ -15,36 +15,52 @@ public class MyStack
         {
             Console.WriteLine("Stack is empty");
         }
-        
-        bool containsSearch = aStack.Contains(search);
+
+        bool containsSearch = false;
+        Stack<string> tempStack = new Stack<string>();
+
+        // Search for the item to remove
+        while (aStack.Count > 0)
+        {
+            string item = aStack.Pop();
+            if (item == search)
+            {
+                containsSearch = true;
+                break;
+            }
+            tempStack.Push(item);
+        }
+
+        // Put the remaining items back on the stack
+        while (tempStack.Count > 0)
+        {
+            aStack.Push(tempStack.Pop());
+        }
+
+        // Add the new item to the stack
+        aStack.Push(newItem);
+
         Console.WriteLine("Stack contains " + search + ": " + containsSearch);
 
         if (containsSearch)
         {
-            Stack<string> tempStack = new Stack<string>();
-            bool foundSearch = false;
+            // Remove the item and all items above it
+            tempStack = new Stack<string>();
 
-            while (aStack.Count > 0)
+            while (aStack.Peek() != search)
             {
-                string item = aStack.Pop();
-                if (item == search)
-                {
-                    foundSearch = true;
-                    break;
-                }
-                tempStack.Push(item);
+                tempStack.Push(aStack.Pop());
             }
 
-            if (!foundSearch)
+            aStack.Pop(); // Remove the search item
+
+            // Put the remaining items back on the stack
+            while (tempStack.Count > 0)
             {
-                while (tempStack.Count > 0)
-                {
-                    aStack.Push(tempStack.Pop());
-                }
+                aStack.Push(tempStack.Pop());
             }
         }
 
-        aStack.Push(newItem);
         return aStack;
     }
 }
