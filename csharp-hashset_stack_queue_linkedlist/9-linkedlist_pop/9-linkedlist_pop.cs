41db445638b835a44ba4a68e8fabc746e95e87f9
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-11 15:00:45 -0500

    Task 9

---
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        if (myLList.Count == 0)
+        {
+            return 0;
+        }
+
+        int data = myLList.First.Value;
+        myLList.RemoveFirst();
+        return data;
+    }
+}
+
