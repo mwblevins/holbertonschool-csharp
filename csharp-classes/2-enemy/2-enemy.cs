96948439cfe26f555bfe28f594f71d4e166237cb
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-19 14:55:32 -0500

    Task 2

--- csharp-classes/2-enemy/2-enemy.cs
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -27,7 +27,7 @@ namespace Enemies
         {
             if (value < 0)
             {
-                throw ArgumentException("Health must be greater than or equal to 0");
+                throw new ArgumentException("Health must be greater than or equal to 0");
             }
         }
 
