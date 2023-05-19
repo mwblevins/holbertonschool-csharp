920e360b6995a6e1bfd1aff0760355ee0baabd43
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-19 14:53:27 -0500

    Task 2

---
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Second namespace in C# named Enemies which includes
+    /// a class of Zombie and its definition
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// First class Zombie with int health 
+        /// </summary>
+        public int health;
+            /// <summary>
+            /// Constructor that sets health to 0 
+            /// </summary>
+
+        public Zombie()
+        {
+            health = 0;
+        }
+            /// <summary>
+            /// Health must be greater than 0 or dead
+            /// </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw ArgumentException("Health must be greater than or equal to 0");
+            }
+        }
+
+    }
+}
