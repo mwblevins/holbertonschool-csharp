9bfc394dba6128ece20ce6bbcb2aedce5b9773b3
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-19 14:39:29 -0500

    Task 1

---
+++ csharp-classes/1-enemy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Second namespace in C# named Enemies which includes
+    /// a class of Zombie and its definition
+    /// </summary>
+    public class Zombie
+    {
+        public int health;
+
+        public Zombie()
+        {
+            health = 0;
+        }
+
+    }
+}
