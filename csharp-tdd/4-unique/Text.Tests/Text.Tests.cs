7f1f696c24c4a377993ec25a9e3203ecb3319fac
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-22 17:53:04 -0500

    tests

--- csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
+++ csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -1,15 +1,27 @@
-namespace Text.Tests;
+using NUnit.Framework;
 
-public class Tests
+namespace Text.Tests
 {
-    [SetUp]
-    public void Setup()
+    public class Tests
     {
-    }
+        const string TestString = "abc", TestString2 = "llama",
+            TestString3 = "racecar", TestString4 = "aabbaa";
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase(TestString, ExpectedResult = 0)]
+        [TestCase(TestString2, ExpectedResult = 3)]
+        [TestCase(TestString3, ExpectedResult = 3)]
+        [TestCase(TestString4, ExpectedResult = -1)]
+        [TestCase("", ExpectedResult = -1)]
+        [TestCase(null, ExpectedResult = -1)]
+        public int failCases(string value) 
+        {
+            return (Str.UniqueChar(value));
+        }
     }
 }
