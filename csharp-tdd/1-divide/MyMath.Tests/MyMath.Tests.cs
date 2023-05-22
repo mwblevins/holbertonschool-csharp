06ea9ff23b5f0c079b70322dbe8e1090408fd33f
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-22 17:50:53 -0500

    Tests

--- csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -1,20 +1,52 @@
-namespace MyMath.Tests;
-/// <summary>
-///Tests 
-/// </summary>
-public class Tests
-{
-    /// <summary>
-    /// tests 
-    /// </summary>
-    [SetUp]
-    public void Setup()
-    {
-    }
+using NUnit.Framework;
+using MyMath;
 
-    [Test]
-    public void Test1()
+namespace MyMath.Tests
+{
+    [TestFixture]
+    public class MatrixTests
     {
-        Assert.Pass();
+        [Test]
+        public void Divide_MatrixIsNull_ReturnsNull()
+        {
+            // Arrange
+            int[,] matrix = null;
+            int num = 5;
+
+            // Act
+            int[,] result = Matrix.Divide(matrix, num);
+
+            // Assert
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void Divide_DivisorIsZero_ReturnsNull()
+        {
+            // Arrange
+            int[,] matrix = new int[,] { { 1, 2 }, { 3, 4 } };
+            int num = 0;
+
+            // Act
+            int[,] result = Matrix.Divide(matrix, num);
+
+            // Assert
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void Divide_DivisorIsPositive_ReturnsCorrectResult()
+        {
+            // Arrange
+            int[,] matrix = new int[,] { { 4, 8 }, { 12, 16 } };
+            int num = 2;
+            int[,] expected = new int[,] { { 2, 4 }, { 6, 8 } };
+
+            // Act
+            int[,] result = Matrix.Divide(matrix, num);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
     }
 }
