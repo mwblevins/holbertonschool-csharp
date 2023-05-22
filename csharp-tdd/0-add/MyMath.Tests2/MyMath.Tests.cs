4b82cb645b5884393e75df6af3d51a6bb550ae4d
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-22 16:27:49 -0500

    trying to fix

---
+++ csharp-tdd/0-add/MyMath.Tests2/MyMath.Tests.cs
@@ -0,0 +1,20 @@
+namespace MyMath.tests;
+/// <summary>
+/// Do all of these need these 
+/// </summary>
+public class Tests
+{
+    /// <summary>
+    /// Tests 
+    /// </summary>
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
