7f7d03b113fd6783c67312ab98a3c104c34df53d
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-22 17:38:22 -0500

    changes again

--- csharp-tdd/2-max_int/MyMath/MyMath.cs
+++ csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -1,30 +1,48 @@
-﻿using System;
+﻿using System.Collections.Generic;
+
 namespace MyMath
 {
-    /// <summary>
-    /// Task 2 idk whats wrong 
-    /// </summary>
-    public class Operations
+    ///<summary> Ncmon dont fail me now </summary>
+    public static class Matrix
     {
-        /// <summary>
-        /// Max number in list, idk how much i need for it to pass or where to put it.
-        /// </summary>
-        /// <param name="nums"></param>
-        /// <returns>max</returns>
-        public static int Max(List<int> nums)
-        {
-            if (nums.Count == 0)
-                return 0;
-
-            int max = nums[0];
-            for (int i = 1; i < nums.Count; i++)
+        ///<summary> copied from oggal to try and see if XML works </summary>
+        public static int [,] Divide(int [,] matrix, int num){
+            if (num == 0)
             {
-                if (nums[i] > max)
-                    max = nums[i];
+                System.Console.WriteLine("Num cannot be 0");
+                return null;
             }
+            if (matrix == null)
+                return null;
+            
+            int[,] res = new int[matrix.GetLength(0),matrix.GetLength(1)];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    res[i,j] = matrix[i,j]/num;
 
-            return max;
+            return res;
+            
         }
     }
 
+    ///<summary> task0 </summary>
+    public static class Operations
+    {
+
+        ///<summary> max int from List </summary>
+        public static int Max(List<int> nums)
+        {
+            int? res = null;
+            if (nums != null)
+            {
+                foreach(int i in nums)
+                {
+                    if (!(res >= i))
+                        res = i;
+                }
+            }
+            return res.GetValueOrDefault();
+        }
+
+    }
 }
