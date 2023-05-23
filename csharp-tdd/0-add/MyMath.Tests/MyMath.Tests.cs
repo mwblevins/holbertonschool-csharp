b4d3a802ca0f2134ac31d03460936946ab06e078
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-23 13:05:44 -0500

    ttests?

--- csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -1,51 +1,20 @@
 using NUnit.Framework;
 using MyMath;
-
 namespace MyMath.Tests
 {
-    [TestFixture]
-    public class OperationsTests
+    public class Tests
     {
-        [Test]
-        public void Add_WithPositiveNumbers_ReturnsCorrectSum()
+        [SetUp]
+        public void Setup()
         {
-            // Arrange
-            int a = 5;
-            int b = 3;
-
-            // Act
-            int result = Operations.Add(a, b);
-
-            // Assert
-            Assert.AreEqual(8, result);
         }
 
         [Test]
-        public void Add_WithNegativeNumbers_ReturnsCorrectSum()
+        public void SimpleAddition()
         {
-            // Arrange
-            int a = -10;
-            int b = -7;
-
-            // Act
-            int result = Operations.Add(a, b);
-
-            // Assert
-            Assert.AreEqual(-17, result);
-        }
-
-        [Test]
-        public void Add_WithZero_ReturnsSameNumber()
-        {
-            // Arrange
-            int a = 15;
-            int b = 0;
-
-            // Act
-            int result = Operations.Add(a, b);
-
-            // Assert
-            Assert.AreEqual(a, result);
+            int res = MyMath.Operations.Add(1, 1);
+            Assert.AreEqual(2, res);
+            Assert.Pass();
         }
     }
 }
