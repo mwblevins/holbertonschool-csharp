954b644fbd10c81264944cf1129565b584927c15
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-31 16:28:56 -0500

    task 2

--- csharp-interfaces/2-doors/2-doors.cs
+++ csharp-interfaces/2-doors/2-doors.cs
@@ -1,12 +1,76 @@
 ﻿using System;
-
-namespace _2_doors
+/// <summary>
+/// public class base
+/// </summary>
+public abstract class Base
 {
-    class Program
+    /// <summary>
+    /// public name
+    /// </summary>
+    public string name { get; set; }
+    /// <summary>
+    /// ToString override
+    /// </summary>
+    public override string ToString()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        return $"{name} is a {GetType().Name}";
+    }
+}
+/// <summary>
+/// Interactive
+/// </summary>
+public interface IInteractive
+{
+    /// <summary>
+    ///interact 
+    /// </summary>
+    void Interact();
+}
+/// <summary>
+/// breakable
+/// </summary>
+public interface IBreakable
+{
+    /// <summary>
+    /// durablity
+    /// </summary>
+    int durability { get; set; }
+    /// <summary>
+    /// break
+    /// </summary>
+    void Break();
+}
+/// <summary>
+/// collect
+/// </summary>
+public interface ICollectable
+{
+    /// <summary>
+    /// these actually clutter the code so much. Bad practice
+    /// </summary>
+    bool isCollected { get; set; }
+    /// <summary>
+    /// collect
+    /// </summary>
+    void Collect();
+}
+/// <summary>
+///door class 
+/// </summary>
+public class Door : Base, IInteractive
+{
+    /// <summary>
+    /// sets name of door and default
+    /// </summary>
+    public Door(string name = "Door")
+    {
+        this.name = name;
+    }
+    /// <summary>
+    ///interact with door 
+    /// </summary>
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {name}. It's locked.");
     }
 }
