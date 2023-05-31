04e01643bb181c72321724998a2bfb6dcd43755d
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-31 16:38:29 -0500

    XML

--- csharp-interfaces/4-keys/4-keys.cs
+++ csharp-interfaces/4-keys/4-keys.cs
@@ -1,15 +1,24 @@
 ﻿using System;
-
+/// <summary>
+/// class Key
+/// </summary>
 class Key : Base, ICollectable
 {
+    /// <summary>
+    /// Collected
+    /// </summary>
     public bool isCollected { get; set; }
-
+    /// <summary>
+    /// key constructor
+    /// </summary>
     public Key(string name = "Key", bool iscollected = false)
     {
         this.name = name;
         this.isCollected = isCollected;
     }
-
+    /// <summary>
+    /// collect method
+    /// </summary>
     public void Collect()
     {
         if (this.isCollected == false)
