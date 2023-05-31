7667102a28e9e754aa1a92c1d8dbd5d317b6e97a
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-31 16:55:00 -0500

    Start

---
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+/// <summary>
+/// Represents a player with a name, maximum health, and current health.
+/// </summary>
+public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    /// <summary>
+    /// Initializes a new instance of the Player class with the specified name and maximum health.
+    /// </summary>
+    /// <param name="name">The name of the player.</param>
+    /// <param name="maxHp">The maximum health of the player.</param>
+    public Player(string name, float maxHp)
+    {
+        this.name = name;
+        if (maxHp > 0)
+        {
+            this.maxHp = maxHp;
+        }
+        else
+        {
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        this.hp = this.maxHp;
+    }
+
+    /// <summary>
+    /// Prints the current health of the player.
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+}
