fc3db518d5e9e9fe77efcda7c2ba4ea33f58902b
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-31 16:12:25 -0500

    task 1

--- csharp-interfaces/1-user_interface/1-user_interface.cs
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,12 +1,64 @@
 ﻿using System;
-
-namespace _1_user_interface
+/// <summary>
+/// public class base
+/// </summary>
+public abstract class Base
 {
-    class Program
+    /// <summary>
+    /// public name
+    /// </summary>
+    public string name { get; set; }
+    /// <summary>
+    /// ToString override
+    /// </summary>
+    public override string ToString()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        return $"{name} is a {GetType().Name}";
+    }
+}
+/// <summary>
+/// Interactive
+/// </summary>
+public interface IInteractive
+{
+    void Interface();
+}
+/// <summary>
+/// breakable
+/// </summary>
+public interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+/// <summary>
+/// collect
+/// </summary>
+public interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+/// <summary>
+///Test class 
+/// </summary>
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+
+    public void Interact()
+    {
+
+    }
+
+    public void Break()
+    {
+
+    }
+
+    public void Collect()
+    {
+
     }
 }
