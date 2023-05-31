edb8c82676b6b622cf0bcbba00b1c8d47b97eee8
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-05-31 16:14:01 -0500

    removed my test

--- csharp-interfaces/1-user_interface/1-user_interface.cs
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -39,26 +39,3 @@ public interface ICollectable
     bool isCollected { get; set; }
     void Collect();
 }
-/// <summary>
-///Test class 
-/// </summary>
-public class TestObject : Base, IInteractive, IBreakable, ICollectable
-{
-    public int durability { get; set; }
-    public bool isCollected { get; set; }
-
-    public void Interact()
-    {
-
-    }
-
-    public void Break()
-    {
-
-    }
-
-    public void Collect()
-    {
-
-    }
-}
